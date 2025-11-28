839881db90a5337c13c3d696dcf65cf7b0e8fa88
Sabrine-Chouaib <sabreen.chouaib@gmail.com>
2025-11-28 22:58:21 +0100

    folder structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Creating directory
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/_init _. py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+I
+
+echo "Directory and files created successfully !. "
